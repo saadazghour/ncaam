4094ee69b1f4a68d8f791623f9db6ee237d99a00
saadazghour <saad.azghour@gmail.com>
2023-07-04 19:33:54 +0100

    Add Dynamic data

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,15 @@
 import Image from "next/image";
 
-const Row = () => {
+const Row = ({ image, name, score, win }) => {
   return (
     <div className="flex justify-between px-4 py-2 border-b border-gray-300">
       <div className="flex">
-        <Image
-          src="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
-          alt="Iowa State Cyclones"
-          width={20}
-          height={20}
-        />
-        <p className="ml-4 font-semibold">Iowa State Cyclones</p>
+        <Image src={image} alt="Iowa State Cyclones" width={20} height={20} />
+        <p className="ml-4 font-semibold">{name}</p>
       </div>
       <div className="flex text-right">
-        <p className="text-gray-700">88-39</p>
-        <p className="ml-2 font-semibold text-green-700">W</p>
+        <p className="text-gray-700">{score}</p>
+        <p className="ml-2 font-semibold text-green-700">{win}</p>
       </div>
     </div>
   );
@@ -23,9 +18,12 @@ const Row = () => {
 export default function Home() {
   return (
     <>
-      <Row />
-      <Row />
-      <Row />
+      <Row
+        name="Iowa State Cyclones"
+        image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+        score="88-39"
+        win="W"
+      />
     </>
   );
 }
