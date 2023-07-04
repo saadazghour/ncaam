d303e6c56fe6031342d862851b6c2e307e80928a
saadazghour <saad.azghour@gmail.com>
2023-07-04 16:44:52 +0100

    Optimize images from remote locations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // Example: https://a.espncdn.com/i/teamlogos/ncaa/500/66.png
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.espncdn.com",
+        port: "",
+        // pathname: "/i/teamlogos/ncaa/500/*/*",
+        pathname: "/i/teamlogos/ncaa/**",
+      },
+    ],
+    formats: ["image/webp", "image/avif"],
+  },
+  experimental: {
+    appDir: true,
+    allowFutureImage: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
