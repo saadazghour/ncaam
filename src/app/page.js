ef2fcdf0cb3781d502ee48b8b7132628d9278df8
saadazghour <saad.azghour@gmail.com>
2023-07-07 20:12:45 +0100

    Add formattedDate

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,38 @@
 import Image from "next/image";
 
-const Row = ({ image, name, score, win }) => {
+const Row = ({ image, name, score, win, date }) => {
+  // Convert the ISO date string to a readable date.
+  // Format the date like this: 11/27 9PM.
+
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "2-digit",
+    hour: "2-digit",
+    // minute: "2-digit",
+  });
+
   return (
-    <div className="flex justify-between px-4 py-2 border-b border-gray-300">
+    <div className="flex justify-between px-8 py-2 border-b border-gray-300">
       <div className="flex">
-        <Image src={image} alt="Iowa State Cyclones" width={20} height={20} />
+        <Image
+          src={image}
+          alt="Iowa State Cyclones"
+          width={20}
+          height={20}
+          className="w-5 h-5"
+        />
         <p className="ml-4 font-semibold">{name}</p>
       </div>
       <div className="flex text-right">
-        <p className="text-gray-700">{score}</p>
-        {win ? (
-          <p className="ml-2 font-semibold text-green-700">W</p>
+        <p className="text-gray-700"> {score}</p>
+        {score ? (
+          win ? (
+            <p className="ml-2 font-semibold text-green-700">W</p>
+          ) : (
+            <p className="ml-2 font-semibold text-red-700">L</p>
+          )
         ) : (
-          <p className="ml-2 font-semibold text-red-700">L</p>
+          <p className="text-gray-700">{formattedDate}</p>
         )}
       </div>
     </div>
@@ -21,6 +41,7 @@ const Row = ({ image, name, score, win }) => {
 
 // This instence of the page component is server components by default.
 export default async function Home() {
+  // This fetch is Equivilance to getStaticProps in NextJS.
   const response = await fetch(
     "https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/66/schedule"
   );
@@ -43,10 +64,11 @@ export default async function Home() {
     return competitors.map((competitor) => {
       return {
         id: competitor.team.id,
+        date: event.competitions[0].date,
         name: competitor.team.displayName,
         logo: competitor.team.logos[0].href,
         score: competitor.score.value,
-        win: competitor.winner,
+        win: winner,
       };
     });
 
@@ -62,11 +84,20 @@ export default async function Home() {
 
   return (
     <>
-      {events.slice(0, 5).map((event) => {
-        const { id, name, logo, score, win } = event[1]; // we only want the second competitor
+      <h2 className="ml-4 text-xl font-semibold">Schedule</h2>
+      <h3 className="mb-2 ml-4 font-semibold text-gray-700">Full</h3>
+      {events.map((event) => {
+        const { id, name, logo, score, win, date } = event[1]; // we only want the second competitor
 
         return (
-          <Row key={id} name={name} image={logo} score={score} win={win} />
+          <Row
+            key={id}
+            name={name}
+            image={logo}
+            date={date}
+            score={score}
+            win={win}
+          />
         );
       })}
     </>
