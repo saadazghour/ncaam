6674a92b12071a173366f5211c3303c6238a419d
saadazghour <saad.azghour@gmail.com>
2023-07-12 18:22:20 +0100

    feat: refactor && Add select component

--- src/app/[teamId]/page.js
+++ src/app/[teamId]/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import TeamSelect from "./select";
-import getScheduleGames from "./espn";
+import { getTeamData, getAllTeamIds } from "../espn";
 
 const Row = ({ id, logo, name, score, winner, date }) => {
   // Convert the ISO date string to a readable date.
@@ -42,12 +42,29 @@ const Row = ({ id, logo, name, score, winner, date }) => {
 };
 
 // This instence of the page component is server components by default.
-export default async function Home() {
-  const games = await getScheduleGames("66");
+export default async function Home({ params }) {
+  const [team, allTeams] = await Promise.all([
+    getTeamData(params.teamId),
+    getAllTeamIds(),
+  ]);
+
+  const { name, record, logo, color, standing, games } = team;
 
   return (
     <>
-      {/* <TeamSelect /> */}
+      <div className="mb-6 border border-gray-300 rounded-md dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+        <select
+          className="w-full px-2 py-3 font-semibold text-gray-800 bg-white border-r-8 rounded-md dark:bg-black dark:text-gray-200 border-r-transparent bg-none"
+          defaultValue={params.teamId}
+        >
+          {allTeams.map(({ id, name }) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <h2 className="ml-4 text-xl font-semibold">Schedule</h2>
       <h3 className="mb-2 ml-4 font-semibold text-gray-700">Full</h3>
 
