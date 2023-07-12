0aeb061ff07ab057a4babbd306b30f450f985261
saadazghour <saad.azghour@gmail.com>
2023-07-12 18:19:13 +0100

    Fetch All teams

--- src/app/espn.js
+++ src/app/espn.js
@@ -1,4 +1,4 @@
-export default async function getScheduleGames(teamId) {
+export async function getTeamData(teamId) {
   // This fetch is Equivilance to getStaticProps in NextJS.
   const res = await fetch(
     `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamId}/schedule`
@@ -6,11 +6,11 @@ export default async function getScheduleGames(teamId) {
 
   const data = await res.json();
 
-  // for all of the events on the schedule
-  // we want to iterate over the competitors and get the name and logo
-  // we also want to get the score and the winner
+  // For all of the events on the schedule
+  // We want to iterate over the competitors and get the name and logo
+  // We also want to get the score and the winner
 
-  return data.events.map((event) => {
+  const games = data.events.map((event) => {
     const { competitors } = event.competitions[0];
 
     const competitorsArray = competitors.map((competitor) => {
@@ -25,8 +25,8 @@ export default async function getScheduleGames(teamId) {
     });
 
     // We are filtering to get some of the data we need.
-    const otherTeam = competitorsArray.find((c) => c.id != 66);
-    const favoriteTeam = competitorsArray.find((c) => c.id == 66);
+    const otherTeam = competitorsArray.find((c) => c.id != teamId);
+    const favoriteTeam = competitorsArray.find((c) => c.id == teamId);
     const score = favoriteTeam.score;
 
     return {
@@ -38,4 +38,42 @@ export default async function getScheduleGames(teamId) {
       winner: favoriteTeam.winner,
     };
   });
+
+  return {
+    name: data.team.displayName,
+    logo: data.team.logo,
+    color: data.team.color,
+    record: data.team.recordSummary,
+    standing: data.team.standingSummary,
+    games,
+  };
+}
+
+export async function getAllTeamIds(teamId) {
+  // Total Pages is the number of teams divided by 100 (max 100 teams per page).
+  // const totalPages = data.sports.leagues[0].teams.length / 100;
+
+  let page = 1;
+  let teams = [];
+
+  while (page <= 8) {
+    const res = await fetch(
+      `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams?page=${page}`
+    );
+
+    const data = await res.json();
+
+    teams.push(
+      ...data.sports[0].leagues[0].teams.map(({ team }) => {
+        return {
+          id: team.id,
+          name: team.displayName,
+        };
+      })
+    );
+
+    page++;
+  }
+
+  return teams;
 }
