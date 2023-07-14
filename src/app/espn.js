2171d332b54337f3f5f793feedc35293e14f682f
saadazghour <saad.azghour@gmail.com>
2023-07-14 20:33:20 +0100

    fix: API incorrect data

--- src/app/espn.js
+++ src/app/espn.js
@@ -1,7 +1,7 @@
 export async function getTeamData(teamId) {
   // This fetch is Equivilance to getStaticProps in NextJS.
   const res = await fetch(
-    `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamId}/schedule`
+    `https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/teams/${teamId}`
   );
 
   const data = await res.json();
@@ -10,32 +10,38 @@ export async function getTeamData(teamId) {
   // We want to iterate over the competitors and get the name and logo
   // We also want to get the score and the winner
 
-  const games = data.events.map((event) => {
-    const { competitors } = event.competitions[0];
-
-    const competitorsArray = competitors.map((competitor) => {
-      return {
-        id: competitor.team.id,
-        date: event.competitions[0].date,
-        name: competitor.team.displayName,
-        logo: competitor.team.logos[0].href,
-        score: competitor.score.value,
-        winner: competitor.winner,
-      };
-    });
+  const games = data.team.nextEvent.map((event) => {
+    const { competitors, date } = event.competitions[0];
 
     // We are filtering to get some of the data we need.
-    const otherTeam = competitorsArray.find((c) => c.id != teamId);
-    const favoriteTeam = competitorsArray.find((c) => c.id == teamId);
-    const score = favoriteTeam.score;
+    const otherTeam = competitors.find((c) => c.team.id != teamId);
+    const favoriteTeam = competitors.find((c) => c.team.id == teamId);
+    // const score = favoriteTeam.score;
+
+    // Unfortunately this data isn't on this API yet
+    // This is for teams with black logos, so we invert the color of the image
+
+    const color =
+      favoriteTeam.team.displayName ===
+      ("Iowa Hawkeyes" || "Long Beach State Beach")
+        ? "000000"
+        : "TODO";
+
+    // Some teams don't have logos, use the default logo.
+    const logo = otherTeam.team.logos
+      ? otherTeam.team.logos[0].href
+      : "https://a.espncdn.com/i/teamlogos/default-team-logo-500.png";
 
     return {
-      id: event.id,
-      date: event.competitions[0].date,
-      name: otherTeam.name,
-      logo: otherTeam.logo,
-      score: score && `${otherTeam.score}-${favoriteTeam.score}`,
+      teamId: otherTeam.team.id,
+      date: date,
+      name: otherTeam.team.displayName,
+      rank: otherTeam.curatedRank.current,
+      homeScore: favoriteTeam.score?.value,
+      awayScore: otherTeam.score?.value,
       winner: favoriteTeam.winner,
+      logo,
+      color,
     };
   });
 
