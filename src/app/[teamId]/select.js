6f5db5fee6e80fb2ef5cd2fabddbff57369904be
saadazghour <saad.azghour@gmail.com>
2023-07-14 13:35:32 +0100

    refactor

--- src/app/[teamId]/select.js
+++ src/app/[teamId]/select.js
@@ -7,13 +7,12 @@ const Select = ({ allTeams, teamId }) => {
   const router = useRouter();
 
   const handleChangeTeam = (e) => {
-    const teamId = e.target.value;
-    router.push(`/${teamId}`);
+    const { value } = e.target;
+    router.push(`/${value}`);
   };
 
   return (
     <div className="mb-6 border border-gray-300 rounded-md dark:border-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-
       <select
         className="w-full px-2 py-3 font-semibold text-gray-800 bg-white border-r-8 rounded-md dark:bg-black dark:text-gray-200 border-r-transparent bg-none"
         defaultValue={teamId}
