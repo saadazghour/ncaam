f0ccbb17e22bd8091b2c150ae14e73f53609da38
saadazghour <saad.azghour@gmail.com>
2023-07-14 20:41:03 +0100

    Remove formattedDate &&  fix props

--- src/app/[teamId]/page.js
+++ src/app/[teamId]/page.js
@@ -2,23 +2,23 @@ import Image from "next/image";
 import TeamSelect from "./select";
 import { getTeamData, getAllTeamIds } from "../espn";
 
-const Row = ({ id, logo, name, score, winner, date }) => {
-  // Convert the ISO date string to a readable date.
-  // Format the date like this: 11/27 9PM.
-
-  const formattedDate = new Date(date).toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    hour: "2-digit",
-    // minute: "2-digit",
-  });
-
+const Row = ({
+  id,
+  logo,
+  name,
+  color,
+  rank,
+  homeScore,
+  awayScore,
+  winner,
+  date,
+}) => {
   return (
     <div className="flex justify-between px-8 py-2 border-b border-gray-300">
       <div className="flex">
         <Image
           src={logo}
-          alt="Iowa State Cyclones"
+          alt={name}
           width={20}
           height={20}
           className="w-5 h-5"
@@ -26,15 +26,20 @@ const Row = ({ id, logo, name, score, winner, date }) => {
         <p className="ml-4 font-semibold">{name}</p>
       </div>
       <div className="flex text-right">
-        <p className="text-gray-700"> {score}</p>
-        {score ? (
+        {homeScore ? (
+          <p className="text-gray-700 dark:text-gray-300">
+            {`${homeScore}-${awayScore}`}
+          </p>
+        ) : null}
+
+        {homeScore ? (
           winner ? (
             <p className="ml-2 font-semibold text-green-700">W</p>
           ) : (
             <p className="ml-2 font-semibold text-red-700">L</p>
           )
         ) : (
-          <p className="text-gray-700">{formattedDate}</p>
+          <p className="text-gray-700 dark:text-gray-300">{date}</p>
         )}
       </div>
     </div>
@@ -48,7 +53,8 @@ export default async function Home({ params }) {
     getAllTeamIds(),
   ]);
 
-  const { name, record, logo, color, standing, games } = team;
+  const { id, name, record, logo, color, standing, games } = team;
+  const [allGames] = games;
 
   return (
     <>
@@ -57,7 +63,7 @@ export default async function Home({ params }) {
       <h2 className="ml-4 text-xl font-semibold">Schedule</h2>
       <h3 className="mb-2 ml-4 font-semibold text-gray-700">Full</h3>
 
-      {games.map((game) => {
+      {allGames.map((game) => {
         return <Row key={game.id} {...game} />;
       })}
     </>
